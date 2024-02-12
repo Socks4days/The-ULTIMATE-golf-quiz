48e340ac475ccd14e08b5a7e60ea6f8943d21c92
Socks4days <144039091+Socks4days@users.noreply.github.com>
2024-02-12 17:59:58 +0000

    Added all Type It Questions Added all Multiple Choice Questions

--- The ULTIMATE golf quiz/Admin/frmViewUsers.Designer.cs
+++ The ULTIMATE golf quiz/Admin/frmViewUsers.Designer.cs
@@ -31,7 +31,6 @@
             this.btnReturn = new System.Windows.Forms.Button();
             this.listViewPlayers = new System.Windows.Forms.ListView();
             this.columnHeaderUsers = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.lblViewPlayers = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnReturn
@@ -44,10 +43,10 @@
             this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReturn.ForeColor = System.Drawing.Color.White;
             this.btnReturn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnReturn.Location = new System.Drawing.Point(12, 11);
-            this.btnReturn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReturn.Location = new System.Drawing.Point(9, 9);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(75, 50);
+            this.btnReturn.Size = new System.Drawing.Size(56, 41);
             this.btnReturn.TabIndex = 8;
             this.btnReturn.Text = "←";
             this.btnReturn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -57,7 +56,7 @@
             // listViewPlayers
             // 
             this.listViewPlayers.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
-            this.listViewPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewPlayers.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.listViewPlayers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(75)))), ((int)(((byte)(0)))));
             this.listViewPlayers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeaderUsers});
@@ -66,10 +65,10 @@
             this.listViewPlayers.ForeColor = System.Drawing.Color.White;
             this.listViewPlayers.GridLines = true;
             this.listViewPlayers.HideSelection = false;
-            this.listViewPlayers.Location = new System.Drawing.Point(289, 78);
-            this.listViewPlayers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listViewPlayers.Location = new System.Drawing.Point(195, 45);
+            this.listViewPlayers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listViewPlayers.Name = "listViewPlayers";
-            this.listViewPlayers.Size = new System.Drawing.Size(205, 370);
+            this.listViewPlayers.Size = new System.Drawing.Size(185, 301);
             this.listViewPlayers.TabIndex = 9;
             this.listViewPlayers.UseCompatibleStateImageBehavior = false;
             this.listViewPlayers.View = System.Windows.Forms.View.Details;
@@ -77,35 +76,21 @@
             // columnHeaderUsers
             // 
             this.columnHeaderUsers.Text = "Players";
-            this.columnHeaderUsers.Width = 250;
-            // 
-            // lblViewPlayers
-            // 
-            this.lblViewPlayers.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblViewPlayers.AutoSize = true;
-            this.lblViewPlayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblViewPlayers.ForeColor = System.Drawing.Color.White;
-            this.lblViewPlayers.Location = new System.Drawing.Point(347, 33);
-            this.lblViewPlayers.Name = "lblViewPlayers";
-            this.lblViewPlayers.Size = new System.Drawing.Size(87, 25);
-            this.lblViewPlayers.TabIndex = 10;
-            this.lblViewPlayers.Text = "All users";
+            this.columnHeaderUsers.Width = 300;
             // 
             // frmViewUsers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.ClientSize = new System.Drawing.Size(760, 473);
-            this.Controls.Add(this.lblViewPlayers);
+            this.ClientSize = new System.Drawing.Size(570, 384);
             this.Controls.Add(this.listViewPlayers);
             this.Controls.Add(this.btnReturn);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmViewUsers";
             this.Text = "frmViewUsers";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -114,6 +99,5 @@
         private System.Windows.Forms.Button btnReturn;
         private System.Windows.Forms.ListView listViewPlayers;
         private System.Windows.Forms.ColumnHeader columnHeaderUsers;
-        private System.Windows.Forms.Label lblViewPlayers;
     }
 }
